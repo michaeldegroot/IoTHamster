4340d3eace60621f98037c6fc5cd6e46f661d567
michaeldegroot <specamps@gmail.com>
2020-01-31 16:19:23 +0100

    un-needed

--- iotdevice/mqtt_iothamster.ino
+++ iotdevice/mqtt_iothamster.ino
@@ -368,9 +368,3 @@ void connectMqtt() {
     }
   }
 }
-void printHex(byte * buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
-  }
-}
